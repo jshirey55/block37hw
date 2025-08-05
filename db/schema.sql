62f34ac032334c23f01e477388daa4910d1a6ec9
Joshua Shirey <jshirey5555@gmail.com>
2025-08-05 14:52:47 -0400

    start

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,27 @@
--- TODO
+DROP DATABASE IF EXISTS jukebox;
+CREATE DATABASE jukebox;
+\c jukebox;
+
+CREATE TABLE playlists (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    description text NOT NULL,
+);
+
+CREATE TABLE playlist_tracks (
+    id serial PRIMARY KEY,
+    playlist_id int NOT NULL,
+    track_id in NOT NULL,
+
+    CONSTRAINT playlist_tracks UNIQUE (playlist_id, track_id)
+);
+
+CREATE TABLE tracks (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    duration_ms int NOT NULL,
+);
+
+
+
+
--- db/seed.js
+++ db/seed.js
@@ -5,6 +5,20 @@ await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
+
+const tracks = [
+  'track1', 'track2', 'track3', 'track4', 'track5',
+  'track6', 'track7', 'track8', 'track9', 'track10',
+  'track11', 'track12', 'track13', 'track14', 'track15',
+  'track16', 'track17', 'track18', 'track19', 'track20',
+];
+
+const playslist = [
+  'playlist1', 'playlist2', 'playlist3', 'playlist4',
+  'playlist5', 'playlist6', 'playlist7', 'playlist8',
+  'playlist9', 'playlist10',
+];
+
 async function seed() {
   // TODO
 }
