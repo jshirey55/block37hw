f07353b814547d0227113cbb4513647b2e4792b5
jshirey55 <jshirey5555@gmail.com>
2025-08-06 00:44:15 -0400

    half passed

--- api/playlists.js
+++ api/playlists.js
@@ -2,16 +2,34 @@ import express from "express"
 const router = express.Router()
 export default router
 
-import { getPlaylistById, getPlaylists } from "#db/queries/playlists"
+import { getPlaylistById, getPlaylists, createPlaylist } from "#db/queries/playlists"
 
-router.route("/").get(async (req, res) => {
+router
+    .route("/")
+    .get(async (req, res) => {
     const playlist = await getPlaylists()
-    res.send(playlist)
+    res.status(200).send(playlist)
+})
+
+.post(async (req, res) => {
+    if(!req.body) return res.status(400).send("Request must have a body")
+            
+    const { name, description } = req.body;
+    if (!name || !description)
+        return res.status(400).send("REQ must have name, desc.")
+
+    const playlist = await createPlaylist({ name, description })
+            res.status(201).send(playlist)
 })
 
 router.param("id", async (req, res, next, id) => {
+    if (!/^\d+$/.test(id))
+        return res.status(400).send("ID must be positive int.")
+    
     const playlist = await getPlaylistById(id)
     if(!playlist) return res.status(404).send("Playlist not found")
+
+    
 
     req.playlist = playlist
     next()
@@ -19,4 +37,8 @@ router.param("id", async (req, res, next, id) => {
 
 router.route("/:id").get((req, res) => {
     res.send(req.playlist)
+})
+
+.put(async (req, res) => {
+    if (!req.body) return res.status(400).json("Request must have a body")
 })
--- api/tracks.js
+++ api/tracks.js
@@ -1,18 +1,25 @@
 import express from "express"
 const router = express.Router()
-export default router
 
 import { getTracks, getTrackById } from "#db/queries/tracks"
 
-router.route("/").get(async (req, res) => {
-    const track = await getTracks()
-    res.send(track)
-})
+router
+    .route("/")
+    .get(async (req, res) => {
+        const tracks = await getTracks()
+        res.send(tracks)
+    })
 
 router.param("id", async (req, res, next, id) => {
-    const track = await getTrackById(id)
-    if (!track) return res.status(404).send("Track not found")
+    if (!/^\d+$/.test(id)) {
+        return res.status(400).send("ID must be positive int.")
+    }
 
+    const track = await getTrackById(id)
+    if (!track) {
+        return res.status(404).send("Track not found")
+    }
+    
     req.track = track
     next()
 })
@@ -20,3 +27,5 @@ router.param("id", async (req, res, next, id) => {
 router.route("/:id").get((req, res) => {
     res.send(req.track)
 })
+
+export default router
